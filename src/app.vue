4ae6d624310d973250626421b3daeae470d47585
FlowerY21 <work_zyue@163.com>
2019-05-21 01:11:07 +0800

    'mpvue项目搭建'

---
+++ src/app.vue
@@ -0,0 +1,10 @@
+
+<script>
+export default {
+    name: "app"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/home/main.js
@@ -0,0 +1,7 @@
+import Vue from  'vue'
+import Home from './home'
+
+const home = new Vue(Home)
+
+//  挂在当前的页面
+home.$mount()
