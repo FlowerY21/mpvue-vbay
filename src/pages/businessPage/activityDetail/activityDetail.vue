96a318438aafea1c3538550ba3a510cbe57a5969
FlowerY21 <work_zyue@163.com>
2019-05-29 14:18:00 +0800

    '活动详情'

---
+++ src/pages/businessPage/activityDetail/activityDetail.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="top-head" :style="'background: url(' + headImg + ') center/cover no-repeat'"></div>
+    <div class="detail-card common-padding">
+      <div class="activity-title">
+        <p>新品试吃活动</p>
+      </div>
+      <div class="flex-row vertical-center">
+        <img src="../../../../static/images/time.png" alt="timeIcon" class="time-icon">
+        <p>05-14 09:00 至 05-16 19:00</p>
+      </div>
+      <div class="flex-row vertical-center">
+        <img src="../../../../static/images/time.png" alt="timeIcon" class="time-icon">
+        <p>05-14 09:00 至 05-16 19:00</p>
+      </div>
+      <div class="flex-row vertical-center">
+        <img src="../../../../static/images/time.png" alt="timeIcon" class="time-icon">
+        <p>05-14 09:00 至 05-16 19:00</p>
+      </div>
+      <div class="flex-row vertical-center">
+        <img src="../../../../static/images/time.png" alt="timeIcon" class="time-icon">
+        <p>05-14 09:00 至 05-16 19:00</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "activityDetail",
+    data() {
+      return {
+        headImg: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .top-head {
+    width: 100%;
+    height: 316rpx;
+  }
+  .detail-card{
+    width:100%;
+    background:#ffffff;
+  }
+</style>
---
+++ src/pages/businessPage/activityDetail/main.js
@@ -0,0 +1,8 @@
+
+import Vue from  'vue'
+import ActivityDetail from './activityDetail'
+
+const activityDetail = new Vue(ActivityDetail)
+
+//  挂在当前的页面
+activityDetail.$mount()
